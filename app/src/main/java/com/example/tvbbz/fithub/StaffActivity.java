4fd6bd8f3a291422453af6a9194b3231dbbe151c
Zachary Parchment <zacharyparch@gmail.com>
2019-01-20 16:56:44 -0500

    Refined Activity Staff Updates.

--- app/src/main/java/com/example/tvbbz/fithub/StaffActivity.java
+++ app/src/main/java/com/example/tvbbz/fithub/StaffActivity.java
@@ -12,12 +12,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class StaffActivity extends AppCompatActivity{
 
     private Button update;
+    private Button signoutbutton;
 
 
     @Override
@@ -34,6 +36,14 @@ public class StaffActivity extends AppCompatActivity{
             }
         });
 
+        signoutbutton = findViewById(R.id.signoutbuttonstaff);
+        signoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signout();
+            }
+        });
+
     }
 
     private void openupdate()
@@ -41,6 +51,15 @@ public class StaffActivity extends AppCompatActivity{
         Intent intent = new Intent(this,UpdateStaffActivity.class);
         startActivity(intent);
     }
+
+    private void signout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 
 
 
