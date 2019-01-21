f6c0db93f1002c91cf58b6037ba59d4bd7fa2219
Zachary Parchment <zacharyparch@gmail.com>
2019-01-20 22:59:29 -0500

    Added Gym Capacity Retrieval

--- app/src/main/java/com/example/tvbbz/fithub/MainActivity.java
+++ app/src/main/java/com/example/tvbbz/fithub/MainActivity.java
@@ -1,21 +1,54 @@
 package com.example.tvbbz.fithub;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private TextView mcapacity;
+
+    private DatabaseReference mdatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View Gym Capacity
+
+        mcapacity = (TextView) findViewById(R.id.gymcapacity);
+        mdatabase = FirebaseDatabase.getInstance().getReference("capacity/capacity");
+
+        mdatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                mcapacity.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        //Sign out
         button = findViewById(R.id.signoutbutton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
