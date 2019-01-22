c1c0e205c4654e22070188ae0cd576e339c16874
Zachary Parchment <zacharyparch@gmail.com>
2019-01-22 11:55:44 -0500

    Added Gen Updates to Home Page

--- app/src/main/java/com/example/tvbbz/fithub/MainActivity.java
+++ app/src/main/java/com/example/tvbbz/fithub/MainActivity.java
@@ -2,27 +2,35 @@ package com.example.tvbbz.fithub;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import com.google.firebase.auth.FirebaseAuth;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private TextView mcapacity;
+    private ListView updateshome;
+
+    private ArrayList<String> allupdates = new ArrayList<>();
 
     private DatabaseReference mdatabase;
+    private DatabaseReference mdatabaseupdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Home");
 
         //View Gym Capacity
-
         mcapacity = (TextView) findViewById(R.id.gymcapacity);
         mdatabase = FirebaseDatabase.getInstance().getReference("capacity/capacity");
 
@@ -40,6 +47,40 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String value = dataSnapshot.getValue(String.class);
                 mcapacity.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //View Gym Updates
+        updateshome = (ListView) findViewById(R.id.genupdatehome);
+        mdatabaseupdates = FirebaseDatabase.getInstance().getReference("updates");
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,allupdates);
+        updateshome.setAdapter(arrayAdapter);
+        mdatabaseupdates.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                allupdates.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
             }
 
             @Override
